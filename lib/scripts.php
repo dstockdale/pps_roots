8c3b5bc3d1647b5b8410cd6ff3c31969483201a0
Dom Stockdale <dom.stockdale@gmail.com>
2013-12-09 08:00:59 +1100

    Styles the side menu a bit

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '427afff6cf1adcc0338646299d12f297');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'abdea0bae312572d372920c0cc19967f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
