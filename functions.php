20c926726391eccdeebfd79ec627e32576e01c93
Dom Stockdale <dom.stockdale@gmail.com>
2013-12-08 12:43:31 +1100

    Adds advanced-custom-fields plugin into the theme

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 /**
  * Roots includes
  */
+require_once locate_template('/lib/custom.php');          // Custom functions
 require_once locate_template('/lib/utils.php');           // Utility functions
 require_once locate_template('/lib/init.php');            // Initial theme setup and constants
 require_once locate_template('/lib/wrapper.php');         // Theme wrapper class
@@ -16,4 +17,3 @@ require_once locate_template('/lib/comments.php');        // Custom comments mod
 require_once locate_template('/lib/relative-urls.php');   // Root relative URLs
 require_once locate_template('/lib/widgets.php');         // Sidebars and widgets
 require_once locate_template('/lib/scripts.php');         // Scripts and stylesheets
-require_once locate_template('/lib/custom.php');          // Custom functions
--- lib/custom.php
+++ lib/custom.php
@@ -2,3 +2,49 @@
 /**
  * Custom functions
  */
+define( 'ACF_LITE', true );
+
+$theme_root = realpath(dirname(__FILE__) . '/..');
+
+include_once($theme_root . '/advanced-custom-fields/acf.php');
+
+if(function_exists("register_field_group"))
+{
+  register_field_group(array (
+    'id' => 'acf_colour-scheme',
+    'title' => 'Colour Scheme',
+    'fields' => array (
+      array (
+        'key' => 'field_52a2548fde314',
+        'label' => 'Colour Scheme',
+        'name' => 'colour_scheme',
+        'type' => 'select',
+        'choices' => array (
+          'Dark on light' => 'Dark on light',
+          'Light on dark' => 'Light on dark',
+        ),
+        'default_value' => 'Dark on light',
+        'allow_null' => 0,
+        'multiple' => 0,
+      ),
+    ),
+    'location' => array (
+      array (
+        array (
+          'param' => 'post_type',
+          'operator' => '==',
+          'value' => 'page',
+          'order_no' => 0,
+          'group_no' => 0,
+        ),
+      ),
+    ),
+    'options' => array (
+      'position' => 'normal',
+      'layout' => 'no_box',
+      'hide_on_screen' => array (
+      ),
+    ),
+    'menu_order' => 99,
+  ));
+}
