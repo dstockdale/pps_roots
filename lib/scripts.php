5e1ea31d99ff6ca267566a6aaafe2476a657ea87
Dom Stockdale <dom.stockdale@gmail.com>
2013-12-08 21:00:45 +1100

    Styles display: inline-block;

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '193dff9a1a1a7f22cb4c05d1f5dac9a6');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4c5b9c7d8d302a665916e4c305a86abf');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
