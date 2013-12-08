ee1d5a66b542aab5c6b4abe36e95c615235197c6
Dom Stockdale <dom.stockdale@gmail.com>
2013-12-08 13:00:52 +1100

    Fixes bottom menu so that you can see it on dark backgrounds

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3e5f30e5a8c0f1f152d434c8d13bb18c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b6b2c6ea23c71d6f88b172bad4f3534c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
